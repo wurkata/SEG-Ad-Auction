a5f69131ba8576ae6204c1269e3d1c2a639c3cb4
Yavor Edipov <yavoredipov@gmail.com>
2019-03-15 20:57:45 +0000

    Checkpoint before implementing JFree-fx

--- src/main/java/view/FXApplication.java
+++ src/main/java/view/FXApplication.java
@@ -14,7 +14,7 @@ public class FXApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/dashboard.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("Ad Auction");
         primaryStage.setScene(new Scene(root, 1200, 800));

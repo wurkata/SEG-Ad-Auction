9c3f7d492112427510d019b1f4302901ab23505d
Chris Wang <chris_wang@icloud.com>
2019-03-20 11:34:15 +0000

    filters
    Took 3 minutes

--- src/main/java/common/Filters/TimeFilter.java
+++ src/main/java/common/Filters/TimeFilter.java
@@ -1,4 +1,31 @@
 package common.Filters;
 
-public class TimeFilter {
+import model.ClickLog;
+import model.ImpressionLog;
+import model.ServerLog;
+
+public class TimeFilter extends Filter {
+    private Long startTime;
+    private Long endTime;
+
+    public TimeFilter(long startTime, long endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public boolean filter(ImpressionLog i) {
+        return withinRange(i.getImpressionDate().getTime());
+    }
+
+    public boolean filter(ClickLog c) {
+        return withinRange(c.getClickDate().getTime());
+    }
+
+    public boolean filter(ServerLog s) {
+        return withinRange(s.getEntryDate().getTime());
+    }
+
+    private boolean withinRange(long currentTime) {
+        return !(startTime != null && startTime > currentTime || endTime != null && endTime < currentTime);
+    }
 }

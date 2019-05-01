57a9e47aff492b2851c7e5194cfdfa369d332198
Yavor Edipov <yavoredipov@gmail.com>
2019-05-01 13:54:54 +0100

    Fix register

--- src/main/java/controller/AccountController.java
+++ src/main/java/controller/AccountController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 import java.util.ResourceBundle;
 
 public class AccountController extends GlobalController implements Initializable {
-    public static boolean online=true;
+    public static boolean online = true;
 
     @FXML
     JFXTextField usernameField;
@@ -62,14 +62,15 @@ public class AccountController extends GlobalController implements Initializable
 
         feedbackMsg.textProperty().setValue("");
 
-        useOfflineBtn.setOnMouseReleased(e -> {
-            try {
-                online=false;
-                goTo("dashboard", (Stage) useOfflineBtn.getScene().getWindow(), new DashboardController(null));
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-        });
+        if (_STATE.equals("Login"))
+            useOfflineBtn.setOnMouseReleased(e -> {
+                try {
+                    online = false;
+                    goTo("dashboard", (Stage) useOfflineBtn.getScene().getWindow(), new DashboardController(null));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            });
 
         usernameField.textProperty().addListener(((observable, oldValue, newValue) -> {
             isValidInputUser = newValue.length() > 2;
